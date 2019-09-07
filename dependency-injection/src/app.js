1d3a4aea23d9933b820561cbed9ddd902462397d
Farooq Mahmud <farooq_am@hotmail.com>
2019-09-07 07:32:52 -0700

    Dependency injection example

--- dependency-injection/src/app.js
+++ dependency-injection/src/app.js
@@ -1,5 +1,5 @@
 const Bank = require('./bank');
 const loggers = require('./loggers');
 
-const bank = new Bank('foo', {logger: loggers.upperCaseLogger});
+const bank = new Bank('foo', {logger: loggers.UpperCaseLogger});
 bank.showConfig();
--- dependency-injection/src/loggers.js
+++ dependency-injection/src/loggers.js
@@ -1,12 +1,39 @@
-const upperCaseLogger = {
-  write: (message) => console.log(`${new Date()} : ${message.toUpperCase()}`),
-};
+/* eslint-disable require-jsdoc */
+class Logger {
+  constructor() {
+    if (this.constructor === Logger) {
+      throw new Error('Cannot instantiate an abstract class.');
+    }
+  }
 
-const lowerCaseLogger = {
-  write: (message) => console.log(`${new Date()} : ${message.toLowerCase()}`),
-};
+  write(message) {
+    if (!this.onWrite) {
+      throw new Error('onWrite function is not implemented.');
+    }
+
+    message = this.onWrite(message);
+    console.log(`${new Date()} : ${message}`);
+  }
+}
+
+class LowerCaseLogger extends Logger {
+  onWrite(message) {
+    return message.toLowerCase();
+  }
+}
+
+class UpperCaseLogger extends Logger {
+  onWrite(message) {
+    return message.toUpperCase();
+  }
+}
+
+const lowerCaseLogger = new LowerCaseLogger();
+const upperCaseLogger = new UpperCaseLogger();
 
 module.exports = {
-  upperCaseLogger,
-  lowerCaseLogger,
+  LowerCaseLogger: lowerCaseLogger,
+  UpperCaseLogger: upperCaseLogger,
 };
+
+
